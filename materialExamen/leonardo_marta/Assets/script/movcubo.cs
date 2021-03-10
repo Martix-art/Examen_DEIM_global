28f3d4bd95f7a3670ce9eb5c34468b4a4679d156
Martix <leo.enc.marta@gmail.com>
2021-03-10 20:06:49 +0100

    movimiento
    movimiento cubo y desaparece cesta

---
+++ materialExamen/leonardo_marta/Assets/script/movcubo.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movcubo : MonoBehaviour
+{
+    [SerializeField] float moveSpeed;
+    [SerializeField] GameObject cesta;
+    private bool quitarCesta = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cesta.SetActive(quitarCesta);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //desactivar la cesta
+        if (cesta.activeInHierarchy == false)
+        {
+            MoverCubo();
+        }
+
+        Cesta();
+
+        
+    }
+    //activar con el boton la cesta
+    void Cesta()
+    {
+        if (Input.GetButtonDown("RB"))
+        {
+            quitarCesta = !quitarCesta;
+            cesta.SetActive(quitarCesta);
+            
+        }
+        
+    }
+
+    void MoverCubo()
+    {
+
+        float desplX = Input.GetAxis("Vertical");
+        if (transform.position.x < -190f && desplX > 0)
+        {
+            desplX = 0f;
+        }
+        else if (transform.position.x > 190f && desplX < 0)
+        {
+            desplX = 0f;
+        }
+
+        float desplZ = Input.GetAxis("Horizontal");
+
+        if (transform.position.z < -190f && desplZ < 0)
+        {
+            desplZ = 0f;
+        }
+        else if (transform.position.z > 190f && desplZ > 0)
+        {
+            desplZ = 0f;
+        }
+        
+        
+        //Movemos la nave mediante el método transform.translate
+        //Lo multiplicamos por deltaTime, el eje y la velocidad de movimiento la nave
+        //Space.World es lo que hace que la nave gire entorno a su eje al realizar los giros
+        transform.Translate(Vector3.left * Time.deltaTime * moveSpeed * desplX);
+        transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed * desplZ);
+    }
+
+}
---
+++ materialExamen/leonardo_marta/Assets/script/ponerHuevos.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ponerHuevos : MonoBehaviour
+{
+    [SerializeField] GameObject huevo;
+    float interval = 10f;
+    public float speed = 3f;
+    private float randomNumber;
+    private Vector3 RandomPos;
+    private Transform InitPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        //Lanzo la corrutina 
+
+        StartCoroutine("InstaHuevo");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        interval -= speed * Time.deltaTime;
+    }
+    IEnumerator InstaHuevo()
+    {
+        randomNumber = Random.Range(-170, 170f);
+        RandomPos = new Vector3(randomNumber, 0, randomNumber);
+        print(RandomPos);
+        Vector3 FinalPos = InitPos.position + RandomPos;
+        Instantiate(huevo, FinalPos, Quaternion.identity);
+        yield return new WaitForSeconds(interval);
+    }
+
+}
